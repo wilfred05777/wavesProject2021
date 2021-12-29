1a7e4c0ef52e8063212e1e733f3980efa81107cb
Wilfred <wilfred05777@gmail.com>
2021-12-29 10:46:47 +0800

    67. Hashing password and generating tokens

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,11 +5,11 @@ const authController = {
     try {
       const { email, password } = req.body;
       const user = await authService.createUser(email, password);
-      res.status(200).send({
-        // email,
-        // password,
+      const token = await authService.genAuthToken(user);
+
+      res.cookie("x-access-token", token).status(200).send({
         user,
-        // ok: "yes",
+        token,
       });
     } catch (error) {
       console.log(error);
--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -4,20 +4,26 @@ const createUser = async (email, password) => {
   try {
     if (await User.emailTaken(email)) {
       console.log("Email already in DB");
-      // throw error;
+    } else {
+      console.log("registered successfully in db");
+      const user = new User({
+        email,
+        password,
+      });
+      await user.save();
+      return user;
     }
-
-    const user = new User({
-      email,
-      password,
-    });
-    await user.save();
-    return user;
   } catch (error) {
     throw error;
   }
 };
 
+const genAuthToken = (user) => {
+  const token = user.generateAuthToken();
+  return token;
+};
+
 module.exports = {
   createUser,
+  genAuthToken,
 };
