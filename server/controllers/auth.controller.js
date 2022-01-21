e8157dd56ef0d9fd1ddecb02ab2cb65918f73a5b
Wilfred <wilfred05777@gmail.com>
2022-01-21 21:12:52 +0800

    73. Creating roles

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -45,6 +45,9 @@ const authController = {
       console.log(userHello);
     } catch (error) {}
   },
+  async dog(req, res, next) {
+    res.json({ ok: "yes" });
+  },
 };
 
 module.exports = authController;
