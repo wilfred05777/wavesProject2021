21a8a0cf743998c6b97fb167b33b019699284e5d
Wilfred <wilfred05777@gmail.com>
2022-01-23 09:38:42 +0800

    81. GET and DELETE by id

--- server/services/brand.service.js
+++ server/services/brand.service.js
@@ -1,3 +1,5 @@
+const httpStatus = require("http-status");
+const { ApiError } = require("../middleware/apiError");
 const { Brand } = require("../models/brand");
 
 const addBrand = async (brandname) => {
@@ -12,6 +14,37 @@ const addBrand = async (brandname) => {
   }
 };
 
+const getBrandById = async (id) => {
+  try {
+    const brand = await Brand.findById(id);
+    if (!brand) {
+      throw new ApiError(httpStatus.NOT_FOUND, "Brand not found");
+    }
+    return brand;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteBrandById = async (id) => {
+  try {
+    const brand = await Brand.findByIdAndRemove(id);
+    return brand;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getBrands = async (id) => {
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   addBrand,
+  getBrandById,
+  deleteBrandById,
+  getBrands,
 };
