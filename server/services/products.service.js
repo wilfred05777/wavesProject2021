b26f8b1c782f6748acab80ec2bf86d8376b17908
Wilfred <wilfred05777@gmail.com>
2022-01-23 11:04:09 +0800

    84. POST product

---
+++ server/services/products.service.js
@@ -0,0 +1,15 @@
+const { Product } = require("../models/product");
+
+const addProduct = async (body) => {
+  try {
+    const product = new Product({
+      ...body,
+    });
+    await product.save();
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { addProduct };
