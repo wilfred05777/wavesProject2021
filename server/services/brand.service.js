159518c249745318e4f4565c63e8d8e402f165a9
Wilfred <wilfred05777@gmail.com>
2022-01-23 10:00:04 +0800

    82. GET all brands

--- server/services/brand.service.js
+++ server/services/brand.service.js
@@ -35,8 +35,18 @@ const deleteBrandById = async (id) => {
   }
 };
 
-const getBrands = async (id) => {
+const getBrands = async (args) => {
   try {
+    let order = args.order ? args.order : "desc";
+    let limit = args.limit ? args.limit : 2;
+    // const brands = await Brand.find({})
+    //   .sort([["_id", "desc"]])
+    //   .limit(4);
+    const brands = await Brand.find({})
+      .sort([["_id", order]])
+      .limit(limit);
+    if (!brands) throw new ApiError(httpStatus.NOT_FOUND, "Brands not found");
+    return brands;
   } catch (error) {
     throw error;
   }
