33cabcf8041531bf7e77ac11569e51c44684c540
Wilfred <wilfred05777@gmail.com>
2022-01-25 06:28:14 +0800

    96. Creating the header and the footer

--- client/src/components/navigation/footer.js
+++ client/src/components/navigation/footer.js
@@ -1,7 +1,64 @@
 import React from "react";
+import ContactsIcon from "@mui/icons-material/ContactMail";
+import TimelapseIcon from "@mui/icons-material/Timelapse";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
 
+// import { useSelector } from "react-redux";
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <footer className="bck_b_dark">
+      <div className="container">
+        <div className="logo">WAVES</div>
+        <div className="wrapper">
+          <div className="left">
+            <h2>Contact information</h2>
+            <div className="business_nfo">
+              <div className="tag">
+                <ContactsIcon />
+                <div className="nfo">
+                  <div>Address</div>
+                  <div>Street Address</div>
+                </div>
+              </div>
+              <div className="tag">
+                <TimelapseIcon />
+                <div className="nfo">
+                  <div>Phone</div>
+                  <div>2323232</div>
+                </div>
+              </div>
+              <div className="tag">
+                <PhoneIcon />
+                <div className="nfo">
+                  <div>Working Hours</div>
+                  <div>9am - 5pm</div>
+                </div>
+              </div>
+              <div className="tag">
+                <EmailIcon />
+                <div className="nfo">
+                  <div>Email</div>
+                  <div>wilfred05777@gmail.com</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <h2>Be the first to know</h2>
+            <div>
+              <div>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- client/src/components/navigation/header.js
+++ client/src/components/navigation/header.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <>Header</>;
+  return (
+    <header className="bck_b_light">
+      <div className="container">
+        <div className="left">
+          <div className="logo">WAVES</div>
+        </div>
+        <div className="right">
+          <div className="top">
+            <>
+              <div className="cart_link">
+                <span>1</span>
+                <Link to="/dashboard/user/user_cart">My cart</Link>
+              </div>
+              <Link to="/dashboard">My Account</Link>
+              <span onClick={() => alert("log out")}>Logout</span>
+              <Link to="/sign_in">Log In</Link>
+            </>
+          </div>
+          <div className="bottom">
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
